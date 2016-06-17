e84b6477e3b7213f9121a10ce5c491908ebedccb
atglenn <adam.glenn@gmail.com>
2016-06-17 11:42:19 -0400

    Removing defer on jquery, adding js tag for all.js

--- javascripts/all.js
+++ javascripts/all.js
@@ -901,15 +901,3 @@ $.fn.setup_navigation = function(settings) {
 		e.stopPropagation();
 	});
 }
-jQuery(document).ready(function() {
-  var adx = "Events are temporarily unavailable. Please check back later.";
-  jQuery.ajax({
-    dataType: 'script',
-    url: '<r:site:data name="calendar_feed" />'
-  });
-  setTimeout(function() {
-    if (typeof response == 'undefined') {
-      jQuery('#adx041233').html(adx);
-    }
-  }, 5000);
-});
